b752c826410956b504e053bee78be9ca0e379506
kamal696 <kamalibrahim397@gmail.com>
2023-04-28 09:51:24 -0400

    add absorbpointer and bloc builder to button to show lodaing

--- lib/cubit/add_not_cubit/add_note_cubit.dart
+++ lib/cubit/add_not_cubit/add_note_cubit.dart
@@ -1,5 +1,7 @@
+// ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
 import 'package:hive/hive.dart';
+// ignore: depend_on_referenced_packages
 import 'package:meta/meta.dart';
 import 'package:notes/constant.dart';
 import 'package:notes/models/note_models.dart';
