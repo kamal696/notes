5b821850c233b425a2c9db66ae21116350f8fdda
kamal696 <kamalibrahim397@gmail.com>
2023-04-28 06:26:03 -0400

    add bloc consumer to bottom sheet

--- lib/views/widgets/custom_show_modal_sheet.dart
+++ lib/views/widgets/custom_show_modal_sheet.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes/cubit/add_not_cubit/add_note_cubit.dart';
 import 'package:notes/views/widgets/body_bottom_sheet.dart';
 
 class CustomAddNoteBottomSheet extends StatelessWidget {
@@ -6,6 +9,17 @@ class CustomAddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomBodyNoteBottomSheet();
+    return BlocConsumer<AddNoteCubit, AddNoteState>(
+      listener: (context, state) {
+        if (state is AddNoteSucsses) {
+          Navigator.pop(context);
+        } else if (state is AddNoteFailure) {}
+      },
+      builder: (context, state) {
+        return ModalProgressHUD(
+            inAsyncCall: state is AddNoteLoading ? true : false,
+            child: const CustomBodyNoteBottomSheet());
+      },
+    );
   }
 }
